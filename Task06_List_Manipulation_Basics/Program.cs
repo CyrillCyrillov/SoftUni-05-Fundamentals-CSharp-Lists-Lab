b809cfd9cf4e228b7d206a9c1162eec37997f9ac
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-17 19:36:23 +0200

    Add files via upload

---
+++ Task06_List_Manipulation_Basics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task06_List_Manipulation_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            string comand = string.Empty;
+
+            while(comand != "end")
+            {
+                string[] rawComand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                comand = rawComand[0];
+                if(comand == "Add")
+                {
+                    //int addNumber = int.Parse(rawComand[1]);
+                    numbers.Add(int.Parse(rawComand[1]));
+                }
+                else if(comand == "Remove")
+                {
+                    numbers.Remove(int.Parse(rawComand[1]));
+                }
+                else if(comand == "RemoveAt")
+                {
+                    numbers.RemoveAt(int.Parse(rawComand[1]));
+                }
+                else if(comand == "Insert")
+                {
+                    numbers.Insert(int.Parse(rawComand[2]), int.Parse(rawComand[1]));
+                }
+            }
+            
+            
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
