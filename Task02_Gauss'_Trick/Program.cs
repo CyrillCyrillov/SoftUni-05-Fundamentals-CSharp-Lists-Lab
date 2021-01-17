c54d8adc156944e422efcf123b9db2769dbf5c54
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-17 19:33:03 +0200

    Add files via upload

---
+++ Task02_Gauss'_Trick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task02_Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+
+            int midleOfList = numbers.Count / 2;
+
+            for (int i = 0; i < midleOfList; i++)
+            {
+                numbers[i] = numbers[i] + numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(String.Join(' ', numbers));
+        }
+    }
+}
